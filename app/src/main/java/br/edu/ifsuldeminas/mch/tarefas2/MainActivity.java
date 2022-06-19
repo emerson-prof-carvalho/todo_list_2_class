7c724c824849412ddcced9e2f5c5af7e813f6318
Emerson Carvalho <emerson@MacBook-Pro-de-Emerson.local>
2022-06-19 09:32:58 -0300

    Last class 13/06

--- app/src/main/java/br/edu/ifsuldeminas/mch/tarefas2/MainActivity.java
+++ app/src/main/java/br/edu/ifsuldeminas/mch/tarefas2/MainActivity.java
@@ -2,13 +2,17 @@ package br.edu.ifsuldeminas.mch.tarefas2;
 
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 
 import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
@@ -22,27 +26,42 @@ public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Começar explicando os mecanismos de binding e navegação
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.toolbar);
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        navController = Navigation.findNavController(this,
+                R.id.nav_host_fragment_content_main);
+        appBarConfiguration = new AppBarConfiguration.Builder(
+                navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Chakar a TaskFragment
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
+                if( navController.getCurrentDestination().getId() == R.id.MainFragment) {
+                    navController.navigate(R.id.action_MainFragment_to_TaskFragment);
+                }
+            }
+        });
+
+        navController.addOnDestinationChangedListener(
+                new NavController.OnDestinationChangedListener() {
+            @Override
+            public void onDestinationChanged(@NonNull NavController navController,
+                                             @NonNull NavDestination navDestination,
+                                             @Nullable Bundle bundle) {
+                binding.fab.setVisibility(FloatingActionButton.VISIBLE);
+                if( navController.getCurrentDestination().getId() != R.id.MainFragment) {
+                    binding.fab.setVisibility(FloatingActionButton.INVISIBLE);
+                }
             }
         });
     }
